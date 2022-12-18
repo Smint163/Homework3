c73ce8eb16eecdd960adaa24f450a16a8934f6eb
Smint <smint-russia@mail.ru>
2022-12-19 00:24:04 +0300

    For checking

---
+++ Exercise19/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет,
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result) && result > 9999 && result < 100000)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("ВЫ ВВЕЛИ НЕКОРРЕКТНОЕ ЧИСЛО");
+        }
+    }
+
+    return result;
+}
+
+int number = GetNumber("Введите пятизначное число");
+
+string str = number.ToString();
+
+if((str[0] == str[4]) && (str[1] == str[3]))
+{
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {number} НЕ является палиндромом");
+}
---
+++ Exercise21/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 21
+Напишите программу, которая принимает на вход координаты двух точек
+и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+Console.WriteLine("Введите координаты точки X1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки Y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки Z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки X2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки Y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки Z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double Distance (int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double result = 0;
+    result = Math.Sqrt(Math.Pow((x1-x2),2)+Math.Pow((y1-y2),2)+Math.Pow((z1-z2),2));
+    return result;
+}
+
+Console.WriteLine($"Расстояние между точками = {Distance(x1,y1,z1,x2,y2,z2)}");
